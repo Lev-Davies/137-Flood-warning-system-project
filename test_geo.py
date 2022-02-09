f9b98e53f9f14dcaea6276b21087d93dded8122e
YianWen <93219686+YianWen@users.noreply.github.com>
2022-02-09 14:34:47 +0000

    Test for geo submitted

--- test_geo.py
+++ test_geo.py
@@ -3,6 +3,7 @@ from floodsystem.geo import station_by_distance
 from floodsystem.geo import stations_within_radius
 from floodsystem.geo import rivers_with_station
 from floodsystem.geo import stations_by_river
+from floodsystem.geo import rivers_by_station_number
 from haversine import haversine
 
 list = build_station_list() 
@@ -46,4 +47,30 @@ def test_alphabetical():
 def test_call_two():
     rivers_and_stations = []
     rivers_and_stations = stations_by_river(list)
+
+# To check whether include more rivers with the same number of stations
+def test_including():
+    same_num_stations = True
+    rivers_station = rivers_by_station_number(list, 9)
+    num_stations = [river[1] for river in rivers_station]
+    if num_stations[8] != num_stations[9]:
+        same_num_stations = False
+    assert same_num_stations == True
+    assert len(rivers_station) == 10
+
+# Check if the numbers are sorted
+def test_sorted_number():
+    sorted_number = True
+    rivers_station = rivers_by_station_number(list, 9)
+    previous_num_stations = 0
+    for num_stations in rivers_station:
+        if previous_num_stations ==0:
+            previous_num_stations = num_stations[1]
+        if previous_num_stations < num_stations[1]:
+            sorted_number = False
+            break
+    assert sorted_number == True
+
+
+
 
